7b4c036bf0d006e854a554150ede91915e3f4a7e
Manish <mahivvit@gmail.com>
2023-02-10 19:29:39 +0530

    final commit

---
+++ array.cpp
@@ -0,0 +1,12 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5};
+    int key=3;
+   for(int i=2;i>=0;i--) cout<<arr[i];
+   for(int i=4;i>=key;i--) cout<<arr[i];
+
+
+return 0;
+}
